b90b776290158ec4465e5815e3b1962bd2afc7e2
Nilesh Maurya <102370201+Nil1403@users.noreply.github.com>
2026-01-06 08:26:03 +0530

    updateded headers

--- shared/api.js
+++ shared/api.js
@@ -1,15 +1,13 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwDZyDzBW4VtLaTXMza6OhVPpOfbRJAP2SBOv0kPQlhvu08Ci08OC6-DvB3_22Lqu3U/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzK_RzNs0g9kGvxXCAjNDvlRntxk4KNEiOPXBit_Ld_bNkrR8115wCZ3QOBcnIXTpZq/exec";
 
 async function apiCall(payload) {
   const response = await fetch(API_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload)
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "text/plain;charset=utf-8"
+    }
   });
-
-  if (!response.ok) {
-    throw new Error("Network error");
-  }
 
   return response.json();
 }
